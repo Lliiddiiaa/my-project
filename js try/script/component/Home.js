60830fac7677789130eda17f4baa61276a9262ae
lidia <7697442@mail.ru>
2022-05-13 23:02:49 +0300

    halk krushit home catalog beginnning

--- js try/script/component/Home.js
+++ js try/script/component/Home.js
@@ -1,3 +1,6 @@
+import {catalogData} from "./catalogApi.js";
+import spinner from "./spinner.js";
+
 function Home(){
     this.title ='Home'
     console.log('Рисуем страницу хоум')
@@ -10,98 +13,91 @@ function Home(){
     // console.log(homeSlider)
     homeSlider.classList.add('home_slider');
     // console.log(header);
+
     const elem = document.createElement('div');
     elem.classList.add('home_component');
-    elem.innerHTML = `
-    <main class="main">
-    <section class="catalog">
-        <div class="wrapper">
-            <div class="catalog-grid">
-                <div class="catalog-grid-item category-item care-category">
-                    <div class="category-desc">
-                        <p>Care</p>
-                        <div class="arrows">
-                            <a href="" class="arrow-left"> <img src="./img/arrow-left.png" alt="arrow-left"> </a>
-                            <a href="" class="arrow-right"> <img src="./img/arrow-right.png" alt="arrow-right"> </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="catalog-grid-item cat-item1">
-                    <div class="card-description">
-                        <p>Perfect brows</p>
-                        <p>54 €</p>
-                    </div>
-                </div>
-                <div class="catalog-grid-item cat-item2">
-                    <div class="card-description">
-                        <p>Perfect brows</p>
-                        <p>54 €</p>
-                    </div>
-                </div>
-                <div class="catalog-grid-item cat-item3">
-                    <div class="card-description">
-                        <p>Perfect brows</p>
-                        <p>54 €</p>
-                    </div>
-                </div>
-                <div class="catalog-grid-item cat-item4">
-                    <div class="card-description">
-                        <p>Perfect brows</p>
-                        <p>54 €</p>
-                    </div>
-                </div>
-                <div class="catalog-grid-item cat-item5">
-                    <div class="card-description">
-                        <p>Perfect brows</p>
-                        <p>54 €</p>
-                    </div>
-                </div>
-                <div class="catalog-grid-item cat-item6">
-                    <div class="card-description">
-                        <p>Perfect brows</p>
-                        <p>54 €</p>
-                    </div>
-                </div>
-                <div class="catalog-grid-item category-item beauty-category">
-                    <div class="category-desc">
-                        <p>Beauty</p>
-                        <div class="arrows">
-                            <a href="" class="arrow-left"> <img src="./img/arrow-left.png" alt="arrow-left"> </a>
-                            <a href="" class="arrow-right"> <img src="./img/arrow-right.png" alt="arrow-right"> </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="catalog-grid-item category-item brows-category">
-                    <div class="category-desc">
-                        <p>Brows</p>
-                        <div class="arrows">
-                            <a href="" class="arrow-left"> <img src="./img/arrow-left.png" alt="arrow-left"> </a>
-                            <a href="" class="arrow-right"> <img src="./img/arrow-right.png" alt="arrow-right"> </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="catalog-grid-item cat-item7">
-                    <div class="card-description">
-                        <p>Perfect brows</p>
-                        <p>54 €</p>
-                    </div>
-                </div>
-                <div class="catalog-grid-item cat-item8">
-                    <div class="card-description">
-                        <p>Perfect brows</p>
-                        <p>54 €</p>
-                    </div>
-                </div>
-                <div class="catalog-grid-item cat-item9">
-                    <div class="card-description">
-                        <p>Perfect brows</p>
-                        <p>54 €</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="about">
+    
+
+    const mainHome = document.createElement('main');
+    mainHome.classList.add('main');
+    elem.append(mainHome);
+
+    const sectionCatalog = document.createElement('section');
+    sectionCatalog.classList.add('catalog');
+    mainHome.append(sectionCatalog);
+
+    const catalogWrapper= document.createElement('div');
+    catalogWrapper.classList.add('wrapper');
+    sectionCatalog.append(catalogWrapper);
+
+    const homeCatalogGrid= document.createElement('div');
+    homeCatalogGrid.classList.add('catalog-grid');
+    catalogWrapper.append(homeCatalogGrid);
+    
+    let dataDb = []; 
+
+    const render = async (data) => {
+        let spin = spinner();
+        catalogWrapper.append(spin);
+        let localCard = localStorage.getItem('card');
+        localCard = JSON.parse(localCard);
+
+        data = await catalogData();
+        catalogWrapper.removeChild(spin);
+        // console.log(dataDb);
+        // console.log(dataDb.data);  
+        data.forEach(d => {
+            let CatalogGridItem = document.createElement('div');
+            CatalogGridItem.classList.add('catalog-grid-item');
+            let imgLinc = document.createElement('a');
+            imgLinc.setAttribute('href', `#productcard/${d.id}`); 
+            let img = document.createElement('img');
+            img.classList.add('card_img');
+            img.setAttribute('src', d.data.img);
+            imgLinc.append(img);
+            let cardDesc = document.createElement('div');
+            cardDesc.classList.add('card_description');
+            let titleLinc = document.createElement('a');
+            titleLinc.setAttribute('href', `#productcard/${d.id}`); 
+            titleLinc.innerText = d.data.name;
+            let title = document.createElement('h2');
+            title.classList.add('card_title');
+            title.append(titleLinc)
+            let priceCard = document.createElement('p');
+            priceCard.classList.add('card_price');
+            priceCard.innerText = d.data.price;
+
+            let btnAdd = document.createElement('button');
+            btnAdd.classList.add('btn-catalog-add');
+            if (localCard && localCard.some(d => d.id === d.id)){
+                btnAdd.innerText = 'Added';
+                btnAdd.disabled = true;
+            }else{
+                btnAdd.innerText = 'Add';
+            }
+
+
+            cardDesc.append(title,priceCard);
+            CatalogGridItem.append(imgLinc, cardDesc, btnAdd);
+            homeCatalogGrid.append(CatalogGridItem);
+            
+            btnAdd.addEventListener('click', () => {
+                import('./Card.js')
+                    .then(module => {
+                        if (module.addCard(d)){
+                            btnAdd.innerText = 'Added';
+                            btnAdd.disabled = true;
+                        }
+                    })
+            })
+        })
+    }
+
+    render(dataDb);
+
+    const sectionAbout = document.createElement('section');
+    sectionAbout.classList.add('about');
+    sectionAbout.innerHTML = `
         <div class="wrapper">
             <div class="about-col1">
                 <img src="./img/about-bg.png" alt="about">
@@ -109,10 +105,10 @@ function Home(){
             <div class="about-col2">
                 <div class="content-wrapper">
                     <div class="logo about_logo"><img src="./img/about-logo.png" alt="logo"></div>
-                    <p class="quote">Each Boltaeva brand product is truly unique - everything is thought out to the smallest detail.</p>
-                    <p class="description">The brand seeks to build respect among the audience for its products, so that the presence of the company's products is a sign of prestige, since the focus of the business is focused on exclusive sales, but not on the mass market.</p>
-                    <div class="signature">
-                        <div class="signature-desription">  
+                        <p class="quote">Each Boltaeva brand product is truly unique - everything is thought out to the smallest detail.</p>
+                        <p class="description">The brand seeks to build respect among the audience for its products, so that the presence of the company's products is a sign of prestige, since the focus of the business is focused on exclusive sales, but not on the mass market.</p>
+                        <div class="signature">
+                            <div class="signature-desription">  
                             <p class="name">Zara Boltaeva</p>
                             <p class="position">CEO, Founder</p>
                         </div> 
@@ -121,45 +117,135 @@ function Home(){
                 </div>
             </div>
         </div>
-    </section>
-    <section class="instagram">
-        <div class="wrapper">
-            <div class="instagram-quote">
-                <p>Boltaeva - this is a real society of girls who show love to themselves</p>
-                <div class="insta-button-wrapper">
-                    <a href="#">Instagram</a>
-                </div>
-            </div>
-            <div class="instagram-grid">
-                <div class="instagram-grid-item inst-item1">
-                    <img src="./img/item1-insta-grid.png" alt="">
-                </div>
-                <div class="instagram-grid-item inst-item2">
-                    <img src="./img/item2-insta-grid.png" alt="">
-                </div>
-                <div class="instagram-grid-item inst-item3">
-                    <img src="./img/item1-insta-grid.png" alt="">
-                </div>
-                <div class="instagram-grid-item inst-item4">
-                    <img src="./img/item2-insta-grid.png" alt="">
-                </div>
-                <div class="instagram-grid-item inst-item5">
-                    <img src="./img/item3-insta-grid.png" alt="">
-                </div>
-                <div class="instagram-grid-item inst-item6">
-                    <img src="./img/item4-insta-grid.png" alt="">
-                </div>
-                <div class="instagram-grid-item inst-item7">
-                    <img src="./img/item3-insta-grid.png" alt="">
-                </div>
-                <div class="instagram-grid-item inst-item8">
-                    <img src="./img/item4-insta-grid.png" alt="">
-                </div>
-            </div>
-        </div>
-    </section>
-</main>
     `
+    mainHome.append(sectionAbout);
+    
+    const sectionInsta = document.createElement('section');
+    sectionInsta.classList.add('instagram');
+    sectionInsta.innerHTML = `
+            <div class="wrapper">
+                <div class="instagram-quote">
+                    <p>Boltaeva - this is a real society of girls who show love to themselves</p>
+                    <div class="insta-button-wrapper">
+                        <a href="#">Instagram</a>
+                    </div>
+                </div>
+                <div class="instagram-grid">
+                    <div class="instagram-grid-item inst-item1">
+                        <img src="./img/item1-insta-grid.png" alt="">
+                    </div>
+                    <div class="instagram-grid-item inst-item2">
+                        <img src="./img/item2-insta-grid.png" alt="">
+                    </div>
+                    <div class="instagram-grid-item inst-item3">
+                        <img src="./img/item1-insta-grid.png" alt="">
+                    </div>
+                    <div class="instagram-grid-item inst-item4">
+                        <img src="./img/item2-insta-grid.png" alt="">
+                    </div>
+                    <div class="instagram-grid-item inst-item5">
+                        <img src="./img/item3-insta-grid.png" alt="">
+                    </div>
+                    <div class="instagram-grid-item inst-item6">
+                        <img src="./img/item4-insta-grid.png" alt="">
+                    </div>
+                    <div class="instagram-grid-item inst-item7">
+                        <img src="./img/item3-insta-grid.png" alt="">
+                    </div>
+                    <div class="instagram-grid-item inst-item8">
+                        <img src="./img/item4-insta-grid.png" alt="">
+                    </div>
+                </div>
+            </div>
+        `
+    mainHome.append(sectionInsta);
+            
+                // <div class="catalog-grid-item category-item care-category">
+                //     <div class="category-desc">
+                //         <p>Care</p>
+                //         <div class="arrows">
+                //             <a href="" class="arrow-left"> <img src="./img/arrow-left.png" alt="arrow-left"> </a>
+                //             <a href="" class="arrow-right"> <img src="./img/arrow-right.png" alt="arrow-right"> </a>
+                //         </div>
+                //     </div>
+                // </div>
+                // <div class="catalog-grid-item cat-item1">
+                //     <div class="card-description">
+                //         <p>Perfect brows</p>
+                //         <p>54 €</p>
+                //     </div>
+                // </div>
+                // <div class="catalog-grid-item cat-item2">
+                //     <div class="card-description">
+                //         <p>Perfect brows</p>
+                //         <p>54 €</p>
+                //     </div>
+                // </div>
+                // <div class="catalog-grid-item cat-item3">
+                //     <div class="card-description">
+                //         <p>Perfect brows</p>
+                //         <p>54 €</p>
+                //     </div>
+                // </div>
+                // <div class="catalog-grid-item cat-item4">
+                //     <div class="card-description">
+                //         <p>Perfect brows</p>
+                //         <p>54 €</p>
+                //     </div>
+                // </div>
+                // <div class="catalog-grid-item cat-item5">
+                //     <div class="card-description">
+                //         <p>Perfect brows</p>
+                //         <p>54 €</p>
+                //     </div>
+                // </div>
+                // <div class="catalog-grid-item cat-item6">
+                //     <div class="card-description">
+                //         <p>Perfect brows</p>
+                //         <p>54 €</p>
+                //     </div>
+                // </div>
+                // <div class="catalog-grid-item category-item beauty-category">
+                //     <div class="category-desc">
+                //         <p>Beauty</p>
+                //         <div class="arrows">
+                //             <a href="" class="arrow-left"> <img src="./img/arrow-left.png" alt="arrow-left"> </a>
+                //             <a href="" class="arrow-right"> <img src="./img/arrow-right.png" alt="arrow-right"> </a>
+                //         </div>
+                //     </div>
+                // </div>
+                // <div class="catalog-grid-item category-item brows-category">
+                //     <div class="category-desc">
+                //         <p>Brows</p>
+                //         <div class="arrows">
+                //             <a href="" class="arrow-left"> <img src="./img/arrow-left.png" alt="arrow-left"> </a>
+                //             <a href="" class="arrow-right"> <img src="./img/arrow-right.png" alt="arrow-right"> </a>
+                //         </div>
+                //     </div>
+                // </div>
+                // <div class="catalog-grid-item cat-item7">
+                //     <div class="card-description">
+                //         <p>Perfect brows</p>
+                //         <p>54 €</p>
+                //     </div>
+                // </div>
+                // <div class="catalog-grid-item cat-item8">
+                //     <div class="card-description">
+                //         <p>Perfect brows</p>
+                //         <p>54 €</p>
+                //     </div>
+                // </div>
+                // <div class="catalog-grid-item cat-item9">
+                //     <div class="card-description">
+                //         <p>Perfect brows</p>
+                //         <p>54 €</p>
+                //     </div>
+                // </div>
+            
+       
+    
+//------------------------------------------------------------------НАДО ПЕРЕПИСАТЬ
+    
     this.init = () => {
         return elem;
     }
